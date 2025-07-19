59ac5ca0771098f7ede2953fae9bcdd368fdaa20
morgan4563 <morganpark4563@gmail.com>
2025-07-19 22:47:07 +0900

    refactor: tableView -> CollectionView로 컨트롤러, 셀 전환

---
+++ SeSAC7HW16/ChatListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChatListCollectionViewCell.swift
+//  SeSAC7HW16
+//
+//  Created by hyunMac on 7/19/25.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
